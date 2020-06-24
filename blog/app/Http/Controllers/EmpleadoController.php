5199c795c2797e5653054f928cb852246a8682e2
romanvazquez <rdjvazqueza@gmail.com>
2020-06-24 01:19:01 -0500

    Creación de Controlador Empleado

---
+++ blog/app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmpleadoController extends Controller
+{
+    public function mostrar(){
+        return "Bienvenido a la muestra";
+    }
+
+    public function registrar(){
+        
+    }
+
+    public function actualizar(){
+        
+    }
+}
